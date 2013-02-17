502f17a0af2caeaf7b1be2dd76541dff673b5b32
klml <m@klml.de>
2013-02-17 23:10:36 +0100

    parser init

--- gidig.py
+++ gidig.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
-# based on http://jblevins.org/log/webpy-markdown ;)
+# based on http://jblevins.org/log/webpy-markdown
 
 import web
 import markdown
-
+import re
 
 templatespath = 'templates'
 sourcepath = '../source/'
 targetpath = '../'
-sourcemarkup = 'md'
-
+sourcemarkup = '.md'
+alwaysonsite_file = 'templates/alwaysonsite.md'
 
 urls = (
+    '/parser/(.*)', 'hashparser',
+    '/(.*)/', 'redirect',
     '/(.*)', 'page',
 )
 
@@ -28,28 +30,53 @@ class redirect:
     def GET(self, path):
         web.seeother('/' + path)
 
+
+class hashparser:
+    def GET(self, url):
+
+        file = open('../source/klml.md', "r")
+
+        conte = ""
+        for line in file:
+            if re.search('#', line):
+                conte += line
+
+        return url + conte
+
+                #~ f_html = open( "../lister.md" ,"w")
+                #~ f_html.write( line )
+                #~ f_html.close()
+
+
+
 class page:
     def GET(self, url):
         # Handle index pages: path/ maps to path/index.txt
         if url == "" or url.endswith("/"):
             url += "index"
 
+        lemma = url ## TODO param
+
         # Each URL maps to the corresponding .txt file in pages/
-        page_file = sourcepath + '%s.' %(url) + markup ## TODO param
+        content_file = sourcepath + lemma + sourcemarkup ## TODO param
 
         # Try to open the text file, returning a 404 upon failure
         try:
-            f = open(page_file, 'r')
+            f = open(content_file, 'r')
+            a = open(alwaysonsite_file, 'r')
 
         except IOError:
             return web.notfound()
 
         # Read the entire file, converting Markdown content to HTML
+        alwaysonsite = a.read()
+        alwaysonsite = md.convert(alwaysonsite)
+
         content = f.read()
         content = md.convert(content)
         # Render the page.html template using the converted content
-        lemma = '%s' %(url) ## TODO param
-        content = render.headerfooter(lemma, content) ## all from config
+
+        content = render.headerfooter(lemma, alwaysonsite, content) ## all from config
         content = content
 
         f_html = open( targetpath + lemma ,"w")
--- templates/alwaysonsite.md
+++ templates/alwaysonsite.md
@@ -1,6 +1,5 @@
-<div class="menu_top" markdown="1">
+
 - [home](home)
 - [Wer](wer)
 - [Wie](wie)
 - [Was](was)
-</div>
