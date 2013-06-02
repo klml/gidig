01523f490dea050ee32f255694656e1e2be25cc2
klml <m@klml.de>
2013-06-02 19:29:57 +0200

    php funct

--- gidig.php
+++ gidig.php
@@ -20,46 +20,50 @@ if ( $SCRIPT != php ) {
 include_once "lib/markdown.php";
 
 
-if ( isset($_POST['lemma']) && isset($_POST['sourcecontent']) ) {
+function safefile() {
+    if ( isset($_POST['lemma']) && isset($_POST['sourcecontent']) ) {
 
-    $lemma = $_POST['lemma'] ;
-    $sourcecontent = $_POST['sourcecontent'] ;
-    
-    $sourcefilename = $SOURCEROOT . $lemma . $MARKUPEXTENSION ; //  "html/" . $_GET['id'] . ".md" ;
+        $lemma = $_POST['lemma'] ;
+        $sourcecontent = $_POST['sourcecontent'] ;
+        
+        $sourcefilename = $SOURCEROOT . $lemma . $MARKUPEXTENSION ; //  "html/" . $_GET['id'] . ".md" ; +paramter
 
-    $writefilehandle = fopen( $sourcefilename ,"w");
-    $written = fwrite($writefilehandle, $sourcecontent);
-    fclose($writefilehandle);
-    // TODO post: commit
-    if ( $written != false ) {
-        echo $lemma .' with ' . $written . ' bytes was written' ;
-    } else {
-        header("Status: 503 Service Unavailable");
-        echo $lemma . "not written";
-        die();
+        $writefilehandle = fopen( $sourcefilename ,"w");
+        $written = fwrite($writefilehandle, $sourcecontent);
+        fclose($writefilehandle);
+        // TODO post: commit
+        if ( $written != false ) {
+            echo $lemma .' with ' . $written . ' bytes was written' ;
+        } else {
+            header("Status: 503 Service Unavailable");
+            echo $lemma . "not written";
+            die();
+        }
     }
-}
+
+} 
 
 
-
-if ( isset($lemma) || isset($_GET['lemma']) ) {
-    if ( isset($_GET['lemma']) ) $lemma = $_GET['lemma'] ;
-
-    $htmlcontent = file_get_contents( $SCRIPTROOT. '/tpl_header.html' ); 
-
-    $alwayscontent = file_get_contents( $SCRIPTROOT. '/tpl_alwaysonsite.md' );  // TODO variabler
-    $htmlcontent .= markdown($alwayscontent);
-    # __TITLE__ # heading in first line
-
-    $sourcecontent = file_get_contents( $SOURCEROOT . $lemma . $MARKUPEXTENSION , true);
-    $htmlcontent .= markdown($sourcecontent); // TODO ? only on generl markdown call for all?
+function parseall() {
+    if ( isset($lemma) || isset($_GET['lemma']) ) {
+        if ( isset($_GET['lemma']) ) $lemma = $_GET['lemma'] ;
     
-    $htmlcontent .= file_get_contents( $SCRIPTROOT. '/tpl_footer.html' , true); 
-
-    $htmlcontent = str_replace( "__LEMMA__" , $lemma , $htmlcontent);
-
-    $writefilehandle = fopen( $WEBROOT . $lemma . $WEBEXTENSION ,"w");
-    fwrite($writefilehandle, $htmlcontent); // TODO callback?
-
-};
+        $htmlcontent = file_get_contents( $SCRIPTROOT. '/tpl_header.html' ); 
+    
+        $alwayscontent = file_get_contents( $SCRIPTROOT. '/tpl_alwaysonsite.md' );  // TODO variabler
+        $htmlcontent .= markdown($alwayscontent);
+        # __TITLE__ # heading in first line
+    
+        $sourcecontent = file_get_contents( $SOURCEROOT . $lemma . $MARKUPEXTENSION , true);
+        $htmlcontent .= markdown($sourcecontent); // TODO ? only on generl markdown call for all?
+        
+        $htmlcontent .= file_get_contents( $SCRIPTROOT. '/tpl_footer.html' , true); 
+    
+        $htmlcontent = str_replace( "__LEMMA__" , $lemma , $htmlcontent);
+    
+        $writefilehandle = fopen( $WEBROOT . $lemma . $WEBEXTENSION ,"w");
+        fwrite($writefilehandle, $htmlcontent); // TODO callback?
+    
+    };
+}
 ?>
